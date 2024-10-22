214494ee0ec8c8f9cf6dabcc3f4f3f891df0cb84
Dedy Surya Prihantoro <44486274+dedysurya13@users.noreply.github.com>
2024-10-22 13:29:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Notes</title>
+  <title>My Notes</title>
   <link rel="shortcut icon" href="dist/img/favicon.ico" type="image/x-icon">
   <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="plugins/summernote/summernote-bs4.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="container mt-5">
-  <h3>Notes</h3>
-  <input type="text" id="searchInput" class="form-control mb-3" placeholder="Cari catatan...">
+  <h3>Catatan</h3>
+  <input type="text" id="searchInput" class="form-control mb-3" placeholder="Cari catatan..." autocomplete="off">
   <div id="noteList" class="mt-4"></div>
   <button class="btn btn-primary rounded-circle" id="addNoteBtn" style="position: fixed; width:70px; height:70px; bottom: 20px; right: 20px;">+</button>
 </div>
@@ -29,11 +29,11 @@
         <input type="hidden" id="noteId">
         <div class="form-group">
           <label for="noteTitle">Judul</label>
-          <input type="text" class="form-control" id="noteTitle">
+          <input type="text" class="form-control" id="noteTitle" autocomplete="off">
         </div>
         <div class="form-group">
           <label for="noteContent">Catatan</label>
-          <textarea id="noteContent" class="form-control"></textarea>
+          <textarea id="noteContent" class="form-control" autocomplete="off"></textarea>
         </div>
       </div>
       <div class="modal-footer">
